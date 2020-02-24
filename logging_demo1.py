fbbe118ccb09e5a87fb483645738ab5cf38890d2
Supriyankumar <89706.kumar@students.itu.edu>
2020-02-23 19:50:26 -0800

    Add files via upload

---
+++ logging_demo1.py
@@ -0,0 +1,12 @@
+
+
+
+import logging
+
+
+logging.basicConfig(filename="test.log",level=logging.DEBUG)
+
+logging.warning("warning message")
+logging.info("info message")
+
+logging.error("error message")
